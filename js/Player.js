2b086c42a4f0e9b34509464c407fecae0aa378d4
Gregory <91101597+Deuntae@users.noreply.github.com>
2022-02-16 19:33:52 -0600

    Add files via upload

--- js/Player.js
+++ js/Player.js
@@ -1,27 +1,71 @@
 class Player {
-  constructor(x, y, width, height) {
-    var options = {
-      isStatic: true
-    };
-
-    this.body = Bodies.rectangle(x, y, width, height, options);
-
-    this.width = width;
-    this.height = height;
-    this.image = loadImage("./assets/player.png");
-
-    World.add(world, this.body);
+  constructor() {
+    this.name = null;
+    this.index = null;
+    this.positionX = 0;
+    this.positionY = 0;
   }
 
+   addPlayer() {
+    var playerIndex = "players/player" + this.index;
+
+    if (this.index === 1) {
+      this.positionX = width / 2 - 100;
+    } else {
+      this.positionX = width / 2 + 100;
+    }
+
+    database.ref(playerIndex).set({
+      name: this.name,
+      positionX: this.positionX,
+      positionY: this.positionY,
+    });
+  }
+  //TA
+  getDistance() {
+    var playerDistanceRef = database.ref("players/player" + this.index);
+    playerDistanceRef.on("value", data => {
+      var data = data.val();
+      this.positionX = data.positionX;
+      this.positionY = data.positionY;
+    });
+  }
  
-  display() {
-    var pos = this.body.position;
-    var angle = this.body.angle;
-    push();
-    translate(pos.x, pos.y);
-    rotate(angle);
-    imageMode(CENTER);
-    image(this.image, 0, 0, this.width, this.height);
-    pop();
+  //Bp
+  getCount() {
+    var playerCountRef = database.ref("playerCount");
+    playerCountRef.on("value", data => {
+      playerCount = data.val();
+    });
   }
-}
+
+  //Bp
+  updateCount(count) {
+    database.ref("/").update({
+      playerCount: count
+    });
+  }
+
+  resetPlayers(count) {
+    database.ref("/").update({
+      players: 0
+    });
+  }
+  //SA
+  update() {
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).update({
+      name: this.name,
+      positionX: this.positionX,
+      positionY: this.positionY,
+     });
+  }
+
+  //Bp
+  static getPlayersInfo() {
+    var playerInfoRef = database.ref("players");
+    playerInfoRef.on("value", data => {
+      allPlayers = data.val();
+    });
+  }
+} 
